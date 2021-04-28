b2d2ba47d31b4cc8d2d9cb8d9cda7a6742edeb72
Simone <startae14@gmail.com>
2021-04-28 17:06:23 +0200

    04: add event handler for the input inside the Search Bar component, to transform the component into a Controlled Component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  state = {
+    term: ''
+  };
+  onInputChange = event => {
+    this.setState({
+      term: event.target.value
+    });
+  };
   render() {
     return (
-      <div>
-        SearchBar
+      <div className="search-bar ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              onChange={this.onInputChange}
+              value={this.state.term}
+              type="text"
+            />
+          </div>
+        </form>
       </div>
     );
   }
